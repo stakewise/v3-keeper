73fd68003751a4f2d223e20a1f80066bb144bc2d
Evgeny Gusarov <evgeny@stakewise.io>
2023-07-11 11:46:21 +0300

    Improve datefmt

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ from src.startup_check import startup_checks
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
-    datefmt='%m-%d %H:%M',
+    datefmt='%Y-%m-%d %H:%M:%S',
     level=LOG_LEVEL,
 )
 
