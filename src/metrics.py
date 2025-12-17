93531eedc26a75d7edcf7d20d553ac95da463e55
cyc60 <avsysoev60@gmail.com>
2025-12-17 13:08:10 +0300

    Metrics: add network label

--- src/metrics.py
+++ src/metrics.py
@@ -2,35 +2,43 @@ from prometheus_client import Counter, Gauge, Info, start_http_server
 
 from src import _get_project_meta
 from src.common.accounts import keeper_account
-from src.config.settings import METRICS_HOST, METRICS_PORT
+from src.config.settings import METRICS_HOST, METRICS_PORT, NETWORK
 
 
 class Metrics:
     def __init__(self) -> None:
-        self.app_version = Info('app_version', 'V3 Keeper version')
-        self.keeper_account = Info('keeper_account', 'V3 Keeper account')
-        self.epoch = Gauge('epoch', 'Chain finalized head: Epoch')
-        self.consensus_block = Gauge('consensus_block', 'Chain finalized head: Consensus Block')
-        self.execution_block = Gauge('execution_block', 'Chain finalized head: Execution Block')
-        self.execution_ts = Gauge('execution_ts', 'Chain finalized head: Execution Timestamp')
+        self.app_version = Info('app_version', 'V3 Keeper version', labelnames=['network'])
+        self.keeper_account = Info('keeper_account', 'V3 Keeper account', labelnames=['network'])
+        self.epoch = Gauge('epoch', 'Chain finalized head: Epoch', labelnames=['network'])
+        self.consensus_block = Gauge(
+            'consensus_block', 'Chain finalized head: Consensus Block', labelnames=['network']
+        )
+        self.execution_block = Gauge(
+            'execution_block', 'Chain finalized head: Execution Block', labelnames=['network']
+        )
+        self.execution_ts = Gauge(
+            'execution_ts', 'Chain finalized head: Execution Timestamp', labelnames=['network']
+        )
         self.oracle_avg_rewards_per_second = Gauge(
             'oracle_avg_rewards_per_second',
             'Oracle AVG rewards per second',
-            labelnames=['oracle_address'],
+            labelnames=['network', 'oracle_address'],
         )
         self.oracle_update_timestamp = Gauge(
             'oracle_update_timestamp',
             'Oracle update timestamp',
-            labelnames=['oracle_address'],
+            labelnames=['network', 'oracle_address'],
         )
-        self.processed_exits = Counter('processed_exits', 'Number of exits keeper processed')
-        self.keeper_balance = Gauge('keeper_balance', 'Keeper balance')
+        self.processed_exits = Counter(
+            'processed_exits', 'Number of exits keeper processed', labelnames=['network']
+        )
+        self.keeper_balance = Gauge('keeper_balance', 'Keeper balance', labelnames=['network'])
 
     def set_app_version(self) -> None:
-        self.app_version.info({'version': _get_project_meta()['version']})
+        self.app_version.labels(network=NETWORK).info({'version': _get_project_meta()['version']})
 
     def set_keeper_account(self) -> None:
-        self.keeper_account.info({'keeper_account': keeper_account.address})
+        self.keeper_account.labels(network=NETWORK).info({'keeper_account': keeper_account.address})
 
 
 metrics = Metrics()
